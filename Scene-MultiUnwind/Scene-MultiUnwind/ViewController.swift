597186d16810d5207c310775f49325a13efb7231
JIINHEO <gjwldls123@gmail.com>
2021-07-29 16:30:19 +0900

    Create : Multi Unwind Segue

---
+++ Scene-MultiUnwind/Scene-MultiUnwind/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 허지인 on 2021/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func gotoPage1(_ sender: UIStoryboardSegue){
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

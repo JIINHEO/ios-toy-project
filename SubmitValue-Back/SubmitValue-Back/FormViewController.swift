afa881e8c40506b6e9053971622d6b9eb616ffff
JIINHEO <gjwldls123@gmail.com>
2021-08-02 21:50:42 +0900

    Update : 저장소를 사용하여 값을 주고 받기

--- SubmitValue-Back/SubmitValue-Back/FormViewController.swift
+++ SubmitValue-Back/SubmitValue-Back/FormViewController.swift
@@ -18,6 +18,8 @@ class FormViewController: UIViewController {
         // 우리가 앞서 정의해 놓은 프로퍼티는 ViewController(자식) 클래스에 정의되어 있음
         // presentingViewController 속성을 통해 이전 화면 객체를 읽어온 다음, ViewController 타입으로 캐스팅한다.
         // 전달해줄 대상 뷰 컨트롤러의 인스턴스를 찾아오는 역할
+        
+        /*
         let preVC = self.presentingViewController
         guard let vc = preVC as? ViewController else {
             return
@@ -27,10 +29,19 @@ class FormViewController: UIViewController {
         vc.paramEmail = self.email.text
         vc.paramUpdate = self.isUpdate.isOn
         vc.paramInterval = self.interval.value
+         */
         
-        NSLog("\(String(describing: self.email.text))")
-        NSLog("asdfasdfasdf")
-        // 이전 화면으로 복귀한다.
+        
+        /*
+         AppDelegate는 앱 전체를 통틀어 하나의 인스턴스만 존재하도록 iOS 시스템에 의해 보장. -> Singleton
+         때문에 AppDelegate 클래스의 인스턴스는 직접 생성할 수 없으며, 아래와 같이 생성되어 있는 인스턴스를 참조해야 함
+         */
+        let ad = UIApplication.shared.delegate as? AppDelegate
+        
+        ad?.paramEmail = self.email.text
+        ad?.paramUpdate = self.isUpdate.isOn
+        ad?.paramInterval = self.interval.value
+
         
         self.presentingViewController?.dismiss(animated: true)
         
--- SubmitValue-Back/SubmitValue-Back/ViewController.swift
+++ SubmitValue-Back/SubmitValue-Back/ViewController.swift
@@ -15,15 +15,16 @@ class ViewController: UIViewController {
     @IBOutlet var resultInterval: UILabel!
     
     // 값을 직접 전달받을 프로퍼티들
-    var paramEmail: String? // 이메일 값을 전달받을 속성
-    var paramUpdate: Bool? // 자동 갱신 여부를 전달받을 속성
-    var paramInterval: Double? // 갱신주기 값을 전달 받을 속성
+//    var paramEmail: String? // 이메일 값을 전달받을 속성
+//    var paramUpdate: Bool? // 자동 갱신 여부를 전달받을 속성
+//    var paramInterval: Double? // 갱신주기 값을 전달 받을 속성
     
     // 화면이 표시될 때마다 실행되는 메소드
     override func viewWillAppear(_ animated: Bool) {
         NSLog("viewWillAppear 실행")
         
         // nil여부를 체크하여 값이 있을 경우에는 해당하는 레이블 객체에 표시해줌
+        /*
         if let email  = paramEmail {
             resultEmail.text = email
         }
@@ -32,7 +33,21 @@ class ViewController: UIViewController {
         }
         if let interval = paramInterval {
             resultInterval.text = "\(Int(interval))분마다"
+        }*/
+        
+        // AppDelegate 객체의 인스턴스를 가져온다.
+        let ad = UIApplication.shared.delegate as? AppDelegate
+        
+        if let email = ad?.paramEmail {
+            resultEmail.text = email
         }
+        if let update = ad?.paramUpdate {
+            resultUpdate.text = update==true ? "자동갱신" : "자동갱신 안함"
+        }
+        if let interval = ad?.paramInterval {//갱신 주기 표시
+            resultInterval.text = "\(Int(interval))분마다"
+        }
+        
     }
     
     

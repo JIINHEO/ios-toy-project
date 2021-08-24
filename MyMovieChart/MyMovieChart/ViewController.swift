d3a1d972a63eabf356e4191406dc2d84d77beea6
JIINHEO <gjwldls123@gmail.com>
2021-08-24 22:12:01 +0900

    Feat : 데이터 세트

---
+++ MyMovieChart/MyMovieChart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieChart
+//
+//  Created by 허지인 on 2021/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

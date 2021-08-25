a47b1ec1d9b860ec68b7821c04b47f274a2e5781
JIINHEO <gjwldls123@gmail.com>
2021-08-25 21:04:47 +0900

    ex) Feat: 커스텀 프로토타입 셀

--- MyMovieChart/MyMovieChart/ListViewController.swift
+++ MyMovieChart/MyMovieChart/ListViewController.swift
@@ -63,7 +63,47 @@ class ListViewController: UITableViewController {
 //        self.list.append(mvo)
 //    }
         
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 주어진 행에 맞는 데이터 소스를 읽어온다.
+        let row = self.list[indexPath.row]
+        // 테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
 
+
+        // 영화제목이 표시될 레이블을 title 변수로 받음
+        let title = cell.viewWithTag(101) as? UILabel
+        // 영화제목이 표시될 레이블을 desc 변수로 받음
+        let desc = cell.viewWithTag(102) as? UILabel
+        // 영화 개봉일이 표시될 레이블을 opendate 변수로 받음
+        let opendate = cell.viewWithTag(103) as? UILabel
+        // 영화 별점이 표시될 레이블 rating 변수로 받음
+        let rating = cell.viewWithTag(104) as? UILabel
+        // 데이터 소스에 저장된 값을 각 레이블 변수에 할당
+        title?.text = row.title
+        desc?.text = row.description
+        opendate?.text = row.opendate
+        rating?.text = "\(row.rating!)"
+        
+        return cell
+    }
     
     
+    
+    
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("선택된 행은 \(indexPath.row) 번째 행입니다.")
+    }
+    
+    
+
 }
---
+++ MyMovieChart/MyMovieChart/MovieCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCell.swift
+//  MyMovieChart
+//
+//  Created by 허지인 on 2021/08/25.
+//
+
+import Foundation
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    
+}

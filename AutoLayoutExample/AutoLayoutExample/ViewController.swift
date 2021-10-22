9d358bed6b6a4324692537bda4a6cb5047e62d8e
JIINHEO <gjwldls123@gmail.com>
2021-10-22 15:45:07 +0900

    Feat: 명언생성기

--- AutoLayoutExample/AutoLayoutExample/ViewController.swift
+++ AutoLayoutExample/AutoLayoutExample/ViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var colorView: UIView!
     
-    
-    @IBAction func tapChangeColorButton(_ sender: UIButton) {
+     @IBAction func tapChangeColorButton(_ sender: UIButton) {
         self.colorView.backgroundColor = UIColor.blue
         print("색상 변경 버튼이 클릭되었음")
     }
---
+++ QuotesGenerator/QuotesGenerator/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by 허지인 on 2021/10/22.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+    
+}
